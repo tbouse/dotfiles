9325a58852d14829d078fc576c5002a4d4240b38
trev <bouse.3@wright.edu>
2024-03-21 23:08:08 -0400

    updated

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ USERDIR=/home/$SUDO_USER
 cd "$(dirname "$0")"
 SCRIPTDIR="$(pwd)" 
 
+echo "USERDIR: $USERDIR"
 
 
 if [[ "$(id -u)" -eq 0 ]]; then #checking if script is running with root permission
@@ -36,6 +37,7 @@ apt_install() {
 		libxcomposite1 \
 		libasound2 \
 		libxi6 \
+		unzip \
 		libxtst6
 	}
 
@@ -47,7 +49,7 @@ anaconda_install() {
 	fi
 	if [[ $(ls Anaconda3*.sh 2> /dev/null == head -1) ]]; then
 		echo "installer found, running it..."
-		bash Anaconda3-2024.02-1-Linux-x86_64.sh -b -p /home/tbouse/anaconda3
+		bash Anaconda3-2024.02-1-Linux-x86_64.sh -b -p $USERDIR/anaconda3
 		echo "PATH=$PATH:$USERDIR/anaconda3/bin" >> $USERDIR/.profile
 	else
 		echo "downloading anaconda installer..."
@@ -56,9 +58,53 @@ anaconda_install() {
 	fi
 }
 
+#install AWS CLI
+awscli_install() {
+	# Check if AWS CLI is already installed
+    if command -v aws &> /dev/null; then
+        echo "[INFO] AWS CLI is already installed."
+        return
+    fi
+	if [[ -e $(ls awsdl/awscliv2.zip 2> /dev/null | head -1) ]]; then
+		echo "AWS CLI V2 installer found, running it"
+		unzip ./awsdl/awscliv2.zip -d awsdl/
+		sudo ./awsdl/aws/install
+	else
+		mkdir awsdl
+		curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awsdl/awscliv2.zip"
+		unzip ./awsdl/awscliv2.zip -d awsdl/
+		sudo ./awsdl/aws/install
+	fi 
+}
+
+#install vundle
+vundle_get() {
+    mkdir -p $USERDIR/.vim/bundle
+    # Clone Vundle repository
+    git clone https://github.com/VundleVim/Vundle.vim.git $USERDIR/.vim/bundle/Vundle.vim
+    
+    # Create a backup of existing .vimrc file
+    if [ -f $USERDIR/.vimrc ]; then
+        mv $USERDIR/.vimrc $USERDIR/.vimrc.backup
+    fi
+    
+    ln -s $USERDIR/dotfiles/vimrc $USERDIR/.vimrc
+    vim +PluginInstall +qall
+}
+
 apt_install
 anaconda_install
+awscli_install
+su $SUDO_USER 
 
+vundle_get
+echo ""
+echo ""
+echo "=-=-=-=-=-=USER DIR $USERDIR"
+# moving bashrc
+ln -sf ~/dotfiles/bashrc ~/.bashrc
 
-bash $USERDIR/dotfiles/bashrc.sh
-echo "EOF"
+bash ~/dotfiles/vundle_install.sh
+echo ""
+echo ""
+echo "=-=-=-=-=-=USER DIR $USERDIR"
